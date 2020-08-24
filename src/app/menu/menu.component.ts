54f9bb1531c0e931f117202050e1be1af0ffbfd3
SezalVerma <sezalverma.s@gmail.com>
2020-08-24 18:34:10 +0530

    Components Part2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,6 +3,51 @@ import { Component, OnInit } from '@angular/core';
 // Dish - name of class specified in 'shared/dish'
 import {Dish} from '../shared/dish';
 
+// const DISHES of type 'array of Dish (imported)'
+const DISHES : Dish[]= [
+  {
+    id: '0',
+    name: 'Uthappizza',
+    image: '/assets/images/uthappizza.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+  },
+  {
+    id: '1',
+    name: 'Zucchipakoda',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    featured: false,
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+  },
+  {
+    id: '2',
+    name: 'Vadonut',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    featured: false,
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+  },
+  {
+    id: '3',
+    name: 'ElaiCheese Cake',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    featured: false,
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+  }
+];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -14,49 +59,9 @@ export class MenuComponent implements OnInit {
      each entry - containes javascript code
      for each Dish entry, specify all properties defined in Dish class in same order
   */
-  dishes : Dish[]=[
-    {
-      id: '0',
-      name: 'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '4.99',
-      // tslint:disable-next-line:max-line-length
-      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-    },
-    {
-      id: '1',
-      name: 'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      featured: false,
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-    },
-    {
-      id: '2',
-      name: 'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      featured: false,
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-    },
-    {
-      id: '3',
-      name: 'ElaiCheese Cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      featured: false,
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-    }
-   ];
+  dishes = DISHES;                       // type- Dish[]
+
+  selectedDish :Dish = DISHES[0];       // type- Dish   , selected - one dish at a time
 
   constructor() { }
 
