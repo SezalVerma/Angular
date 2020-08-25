123eba1aafa7be3c291941a92568d22b5735f986
SezalVerma <sezalverma.s@gmail.com>
2020-08-25 13:01:16 +0530

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,52 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 // Dish - name of class specified in 'shared/dish'
 import {Dish} from '../shared/dish';
-import {DishdetailComponent} from '../dishdetail/dishdetail.component';
+//import {DishdetailComponent} from '../dishdetail/dishdetail.component';
+import {DISHES} from '../shared/dishes';
 
-// const DISHES of type 'array of Dish (imported)'
-const DISHES : Dish[]= [
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-  },
-  {
-    id: '1',
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-  },
-  {
-    id: '2',
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    featured: false,
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-  },
-  {
-    id: '3',
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    featured: false,
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-  }
-];
 
 @Component({
   selector: 'app-menu',
@@ -55,20 +12,23 @@ const DISHES : Dish[]= [
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  /* dishes- name of var
-     Dish[]- array of Dish class imported above
-     each entry - containes javascript code
-     for each Dish entry, specify all properties defined in Dish class in same order
-  */
-  dishes = DISHES;                       // type- Dish[]
 
-  selectedDish :Dish = DISHES[0];       // type- Dish   , selected - one dish at a time
+  //                                       declare vars here
 
-  
+  dishes : Dish[]= DISHES;                                                  // type- Dish[]
+
+  selectedDish :Dish ;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //                                    declare any other handlers 
+
+  //  when onSelect handler is called with a specific dish , that dish would be assigned to selectedDish var 
+  onSelect(dish : Dish){
+    this.selectedDish = dish;                     // this.name-- name var declared in this class
   }
 
 }
